68fa9490f33d71c8398b9a1d8203061c8fea3ae2
Tomasz Dyrda <tomasz.dyrda@100sw.pl>
2022-05-19 21:35:33 +0200

    DataBase Models

---
+++ ProjectInvoice/DataBase/CommodityModel.cs
@@ -0,0 +1,24 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectInvoice.DataBase
+{
+    public class CommodityModel : ViewModelBase
+    {
+        public ObservableCollection<Commodity> Commodities
+        {
+            get => GetValue<ObservableCollection<Commodity>>();
+            set => SetValue(value);
+        }
+
+        public CommodityModel()
+        {
+            Commodities = new ObservableCollection<Commodity>();
+        }
+    }
+}
---
+++ ProjectInvoice/DataBase/ViewModel.cs
@@ -0,0 +1,48 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectInvoice.DataBase
+{
+    public class ViewModel : ViewModelBase
+    {
+        ProjectInvoiceEntities dbcontext;
+
+
+        public ObservableCollection<Commodity> Commodities
+        {
+            get => GetValue<ObservableCollection<Commodity>>();
+            set => SetValue(value);
+        }
+
+        public ObservableCollection<Invoice> Invoices
+        {
+            get => GetValue<ObservableCollection<Invoice>>();
+            set => SetValue(value);
+        }
+
+        public ViewModel()
+        {
+            dbcontext = new ProjectInvoiceEntities();
+
+            if (IsInDesignMode)
+            {
+                Commodities = new ObservableCollection<Commodity>();
+                Invoices = new ObservableCollection<Invoice>();
+            }
+            else
+            {
+                dbcontext.Commodities.Load();
+                Commodities = dbcontext.Commodities.Local;
+                dbcontext.Invoices.Load();
+                Invoices = dbcontext.Invoices.Local;
+            }
+        }
+    }
+}
