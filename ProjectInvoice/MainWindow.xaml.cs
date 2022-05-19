e890a5fe4ef4cf703af5cbca976c11dac169e4dc
Tomasz Dyrda <tomasz.dyrda@100sw.pl>
2022-05-19 21:52:03 +0200

    Project, properties and packages

---
+++ ProjectInvoice/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using DevExpress.Xpf.Core;
+using ProjectInvoice.DataBase;
+using ProjectInvoice.Views;
+using ProjectInvoice.Views.NewContractor;
+using ProjectInvoice.Views.NewOurCompany;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectInvoice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ThemedWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateInvoice(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+            NewInvoice window = new NewInvoice();
+            window.Owner = this;
+            window.ShowDialog();
+
+        }
+
+        private void CreateNewOurCompany(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+            NewOurCompany window = new NewOurCompany();
+            window.Owner = this;
+            window.ShowDialog();
+        }
+
+        private void CreateNewContractor(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+            NewForeignCompany window = new NewForeignCompany();
+            window.Owner = this;
+            window.ShowDialog();
+        }
+    }
+}
